3e04af1b7ce72f392d8c4573cd54a44a713920eb
Felipe Oliveira <100872607+Z-I-NEXUS-I-Z@users.noreply.github.com>
2025-03-05 18:56:26 +0000

    teste

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -4,10 +4,25 @@
 
 int main() {
    
-    char estado;
-    printf ("Digite o estado");
-    scanf (" %s", estado);
-    printf ("o estado é: %s", estado);
+    // Variaveis da primeira carta (representados com o 1 no final do nome)
 
-    return 0;
+    char estado1 [20];
+    int população1;
+    float area1;
+    float pib1;
+    int num_ponto_turistico1;
+
+    // Variaveis da segunda carta (representados com o 2 no final do nome)
+
+    char estado2 [20];
+    int população2;
+    float area2;
+    float pib2;
+    int num_ponto_turistico2;
+
+    // Mensagem de boas vindas com as instruções para cadastrar as 2 primeiras cartas
+
+    printf ("Bem vindo ao Super Trunfo Países, vamos começar caadastrando a primeira carta\n");
+    printf ("Digite o nome do estadp:\n");
+    
 }
